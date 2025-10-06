0429dafb8694a40959e01b784e0b6c48a4135196
christiankmarshall1988-debug <christiankmarshall1988@gmail.com>
2025-10-06 08:56:25 -0600

    website

---
+++ grades.sql
@@ -0,0 +1,9 @@
+ INSERT INTO students (first_name, last_name, 
+enrollment_date) VALUES
+ ('John', 'Doe', 2024-01-03),
+ ('Jane', 'Doe', 2025-12-30),
+ ('Brittany', 'Macbeth', 2023-06-08);
+ INSERT INTO grades (student_id, grade) VALUES
+ (1, 98),
+ (2, 68),
+ (3, 88);
